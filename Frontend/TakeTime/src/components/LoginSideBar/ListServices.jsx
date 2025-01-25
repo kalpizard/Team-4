80ff3c2287cef38b4cb4e78aa2701a30f3df140a
Eli-Jiron <elianguevara28@gmail.com>
2025-01-25 14:00:24 -0600

    Modales de intercambio y donaciones

--- Frontend/TakeTime/src/components/LoginSideBar/ListServices.jsx
+++ Frontend/TakeTime/src/components/LoginSideBar/ListServices.jsx
@@ -1,43 +1,70 @@
-import { useState } from "react";
-import { useServices } from "../../contexts/ServicesContext.jsx";
+import { useState } from 'react';
+import { useServices } from '../../contexts/ServicesContext.jsx';
+import Modal from '../ui/Modal.jsx';
+import { useAuthContext } from '../../contexts/AuthContext.jsx';
 
 export const ListServices = () => {
+  const { userSession } = useAuthContext();
   const { services, error } = useServices();
-  const [DonarHoras, setDonarHoras] = useState('')
+  const [openDonateModal, setOpenDonateModal] = useState(false);
+  const [openTradeModal, setOpenTradeModal] = useState(false);
 
-  const donarHoras = async() => {
-    console.log(DonarHoras);
-    
+  const donarHoras = async (serviceId) => {
+    setOpenDonateModal(!openDonateModal);
+    console.log(serviceId, userSession, 1);
   };
-  const IntercambiarHoras = async () => {
-    console.log(DonarHoras);
-    
+  const intercambiarHoras = async (serviceId) => {
+    setOpenTradeModal(!openTradeModal);
+    console.log(serviceId, userSession);
   };
   return (
-    <div>
-      <h1>Lista de Servicios</h1>
-      {error && <p style={{ color: "red" }}>Error: {error}</p>}
-      <ul>
-        {services.length > 0 ? (
-          services.map((service) => (
-            
-            <li key={service.id}>
-              <br />
-              <h1>Nombre del servicio: {service.nombreServicio}</h1>
-              <h1>Habilidad: {service.habilidad}</h1>
-              <h2>Ubicacion: {service.ubicacion}</h2>
-              <h3>Modalidad: {service.modalidad}</h3>
-              <h5>Horas Disponibles: {service.horasDisponibles}</h5>
-            <hr />
-            <input onClick={donarHoras} type="button" value="Donar horas" /> <h1>77</h1>
-            <input onClick={IntercambiarHoras} type="button" value="Intercambiar horas" />
-            </li>
-            
-          ))
-        ) : (
-          <p>No hay servicios disponibles</p>
-        )}
-      </ul>
-    </div>
+    <>
+      <div>
+        <h1>Lista de Servicios</h1>
+        {error && <p style={{ color: 'red' }}>Error: {error}</p>}
+        <ul>
+          {services.length > 0 ? (
+            services.map((service) => (
+              <li key={service.id}>
+                <br />
+                <h1>Nombre del servicio: {service.nombreServicio}</h1>
+                <h1>Habilidad: {service.habilidad}</h1>
+                <h2>Ubicacion: {service.ubicacion}</h2>
+                <h3>Modalidad: {service.modalidad}</h3>
+                <h5>Horas Disponibles: {service.horasDisponibles}</h5>
+                <hr />
+                <input
+                  onClick={() => donarHoras(service.id)}
+                  type='button'
+                  value='Donar horas'
+                />{' '}
+                <h1>77</h1>
+                <input
+                  onClick={() => intercambiarHoras(service.id)}
+                  type='button'
+                  value='Intercambiar horas'
+                />
+              </li>
+            ))
+          ) : (
+            <p>No hay servicios disponibles</p>
+          )}
+        </ul>
+      </div>
+
+      <Modal
+        Open={openDonateModal}
+        Close={() => setOpenDonateModal(!openDonateModal)}
+      >
+        <p>donar</p>
+      </Modal>
+
+      <Modal
+        Open={openTradeModal}
+        Close={() => setOpenTradeModal(!openTradeModal)}
+      >
+        <p>Intercambiar</p>
+      </Modal>
+    </>
   );
 };
