764c599d15f3692a671cbf5c0d1dbafab3570fa1
DerekNoguera <dereknoguera9@gmail.com>
2025-01-25 21:01:19 -0600

    validaciones d servicios

--- Frontend/TakeTime/src/components/LoginSideBar/ListServices.jsx
+++ Frontend/TakeTime/src/components/LoginSideBar/ListServices.jsx
@@ -1,93 +1,23 @@
-// import { useState } from 'react';
-// import { useServices } from '../../contexts/ServicesContext.jsx';
-// import Modal from '../ui/Modal.jsx';
-// import { useAuthContext } from '../../contexts/AuthContext.jsx';
-
-// export const ListServices = () => {
-//   const { userSession } = useAuthContext();
-//   const { services, error } = useServices();
-//   const [openDonateModal, setOpenDonateModal] = useState(false);
-//   const [openTradeModal, setOpenTradeModal] = useState(false);
-
-//   const donarHoras = async (serviceId) => {
-//     setOpenDonateModal(!openDonateModal);
-//     console.log(serviceId, userSession, 1);
-//   };
-//   const intercambiarHoras = async (serviceId) => {
-//     setOpenTradeModal(!openTradeModal);
-//     console.log(serviceId, userSession);
-//   };
-//   return (
-//     <>
-//       <div>
-//         <h1>Lista de Servicios</h1>
-//         {error && <p style={{ color: 'red' }}>Error: {error}</p>}
-//         <ul>
-//           {services.length > 0 ? (
-//             services.map((service) => (
-//               <li key={service.id}>
-//                 <br />
-//                 <h1>Nombre del servicio: {service.nombreServicio}</h1>
-//                 <h1>Habilidad: {service.habilidad}</h1>
-//                 <h2>Ubicacion: {service.ubicacion}</h2>
-//                 <h3>Modalidad: {service.modalidad}</h3>
-//                 <h5>Horas Disponibles: {service.horasDisponibles}</h5>
-//                 <hr />
-//                 <input
-//                   onClick={() => donarHoras(service)}
-//                   type='button'
-//                   value='Donar horas'
-//                 />{' '}
-//                 <h1>77</h1>
-//                 <input
-//                   onClick={() => intercambiarHoras(service)}
-//                   type='button'
-//                   value='Intercambiar horas'
-//                 />
-//               </li>
-//             ))
-//           ) : (
-//             <p>No hay servicios disponibles</p>
-//           )}
-//         </ul>
-//       </div>
-
-//       <Modal
-//         Open={openDonateModal}
-//         Close={() => setOpenDonateModal(!openDonateModal)}
-//       >
-//         <h1>Hola mundo</h1>
-//         <p>donar</p>
-//         <button>Confirmar</button>
-//       </Modal>
-
-//       <Modal
-//         Open={openTradeModal}
-//         Close={() => setOpenTradeModal(!openTradeModal)}
-//       >
-//         <p>Intercambiar</p>
-//       </Modal>
-//     </>
-//   );
-// };
 import { useState } from "react";
 import { useServices } from "../../contexts/ServicesContext.jsx";
 import Modal from "../ui/Modal.jsx";
 import { useAuthContext } from "../../contexts/AuthContext.jsx";
-import { postTransaction } from "../../services/axios.js"; // Agregar servicio para transacciones
+import { deleteData, getData, postTransaction } from "../../services/axios.js"; // Agregar servicio para transacciones
+import { putData } from "../../services/axios.js";
 
 export const ListServices = () => {
-  const { userSession } = useAuthContext();
-  const { services, error, fetchServices } = useServices(); // fetchServices para actualizar después de cambios
+  const { userSession, update,setUpdate } = useAuthContext();
+  const { services, error, fetchServices, updateService, setUpdateService  } = useServices(); // fetchServices para actualizar después de cambios
   const [openTradeModal, setOpenTradeModal] = useState(false);
   const [selectedService, setSelectedService] = useState(null);
   const [hours, setHours] = useState(0);
   const url = "http://localhost:3001/tratos"
+  const urlusers = "http://localhost:3001/usuarios"
+  const urlservices = 'http://localhost:3001/servicios'
 
   const handleTrade = async () => {
-    if (!selectedService || hours <= 0) return alert("Ingresa horas válidas");
-    console.log(selectedService);
-    
+    if (!selectedService || hours <= 0 || userSession.hora <= 0 )  return alert("No tienes horas disponibles");
+
     const data = {
       serviceId: selectedService.id,
       buyerId: userSession.id,
@@ -97,9 +27,53 @@ export const ListServices = () => {
       hours: hours,
       type: "trade",
     }
+
+    const userBuy = {
+  
+        id: userSession.id,
+        name: userSession.name,
+        email: userSession.email,
+        password: userSession.password,
+        hora: userSession.hora - hours
+    }
+
+    const servicioUpdate = {
+      habilidad: selectedService.habilidad,
+      horasDisponibles: selectedService.horasDisponibles - hours,
+      id: selectedService.id,
+      idUsuario: selectedService.idUsuario,
+      modalidad: selectedService.modalidad,
+      nameUser: selectedService.nameUser,
+      nombreServicio: selectedService.nombreServicio,
+      ubicacion: selectedService.ubicacion
+
+    }
+    
+
     try {
       await postTransaction(url, data);
       alert("Intercambio realizado exitosamente");
+
+      //se me restan las horas al usuario q contrato
+      await putData(urlusers, userSession.id, userBuy)
+      setUpdate(update+1)
+      
+      
+      //suma las horas al vendedor
+      // await putData(urluserSeller,)
+
+
+      // decrementan las horas de el servicio
+      // await putData(urlusers, selectedService.id)
+      await putData(urlservices, selectedService.id, servicioUpdate)
+      console.log(selectedService.horasDisponibles - hours);
+      
+        if(selectedService.horasDisponibles - hours <= 0){
+          await deleteData(urlservices, selectedService.id)
+        }
+      setUpdateService(updateService+1)
+      
+
       setOpenTradeModal(false);
     } catch (error) {
       alert("Error al intercambiar horas: " + error.message);
